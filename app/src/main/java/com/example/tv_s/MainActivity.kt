9dcce9b4b33ec828dc9207988d5b3cd6a70c55c4
Hezron Na <hezron.w12@gmail.com>
2025-08-11 20:10:10 +0300

    M-1

--- app/src/main/java/com/example/tv_s/MainActivity.kt
+++ app/src/main/java/com/example/tv_s/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.tv_s
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(this, 2) // 2 columns
+
+        val data = listOf("Item 1", "Item 2", "Item 3", "Item 4")
+        recyclerView.adapter = GridAdapter(data)
     }
+}
+
+class GridAdapter(private val items: List<String>) :
+    RecyclerView.Adapter<GridAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val image: ImageView = view.findViewById(R.id.itemImage)
+        val text: TextView = view.findViewById(R.id.itemText)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_grid, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text.text = items[position]
+        // If you have images, you can load them with Glide/Picasso here
+        // Glide.with(holder.image.context).load(url).into(holder.image)
+    }
+
+    override fun getItemCount() = items.size
 }
