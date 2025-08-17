15248a50e1d42fe1afaa8d0724b72d0ce883e24d
Hezron Na <hezron.w12@gmail.com>
2025-08-17 22:03:11 +0300

    K-808

--- app/src/main/java/com/example/tv_s/Watch_Page.kt
+++ app/src/main/java/com/example/tv_s/Watch_Page.kt
@@ -1,18 +1,24 @@
 package com.example.tv_s
 
+import android.media.Rating
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.HttpURLConnection
 import java.net.URL
+
+import android.view.View
+
 
 
 class Watch_Page : AppCompatActivity() {
@@ -25,7 +31,7 @@ class Watch_Page : AppCompatActivity() {
         val imdbCode = intent.getStringExtra("imdb_code")
         val type = intent.getStringExtra("type")
 
-        fetchData("1061474", "movie")
+        fetchData(imdbCode.toString(), type.toString())
 
 
         // Show them (for testing)
@@ -38,10 +44,25 @@ class Watch_Page : AppCompatActivity() {
 
     private fun fetchData(id:String, type: String) {
         CoroutineScope(Dispatchers.IO).launch {
+            var tmdbId = id // mutable copy
             try {
+                if (id.startsWith("tt")){
 
+                    val url = "https://api.themoviedb.org/3/movie/$id/external_ids"
+                    val connection = URL(url).openConnection() as HttpURLConnection
+                    connection.requestMethod = "GET"
+                    connection.setRequestProperty("accept", "application/json")
+                    connection.setRequestProperty(
+                        "Authorization",
+                        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjliMmUyN2MxYTZiYzMyMzNhZjE4MzJmNGFjYzg1MCIsIm5iZiI6MTcxOTY3NDUxNy4xOTYsInN1YiI6IjY2ODAyNjk1ZWZhYTI1ZjBhOGE4NGE3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RTms-g8dzOl3WwCeJ7WNLq3i2kXxl3T7gOTa8POcxcw"
+                    )
+                    val response = connection.inputStream.bufferedReader().use { it.readText() }
+                    val jsonObject = org.json.JSONObject(response)
+                    tmdbId = jsonObject.getString("id")
 
-                val url = "https://api.themoviedb.org/3/$type/$id?language=en-US"
+                }
+
+                val url = "https://api.themoviedb.org/3/$type/$tmdbId?language=en-US"
                 val connection = URL(url).openConnection() as HttpURLConnection
                 connection.requestMethod = "GET"
                 connection.setRequestProperty("accept", "application/json")
@@ -52,33 +73,56 @@ class Watch_Page : AppCompatActivity() {
 
                 val response = connection.inputStream.bufferedReader().use { it.readText() }
                 Log.e("DEBUG_Watch", response)
-
-                /*
                 val jsonObject = org.json.JSONObject(response)
-                val dataObject = jsonObject.getJSONObject("data")
-                val moviesArray = dataObject.getJSONArray("movies")
+                Log.e("DEBUG_Watch", jsonObject.toString())
 
 
-                val movies = mutableListOf<MovieItem>()
+                val backdrop_Url = "https://image.tmdb.org/t/p/w500${jsonObject.getString("backdrop_path")}"
+                val poster_Url = "https://image.tmdb.org/t/p/w500${jsonObject.getString("backdrop_path")}"
+                val original_title  = jsonObject.getString("original_title")
+                val overview  = jsonObject.getString("overview")
+                val poster_path  = jsonObject.getString("poster_path")
+                val release_date  = jsonObject.getString("release_date")
+                val runtime  = jsonObject.getString("runtime")
+                val vote_average  = jsonObject.getString("vote_average")
 
-                for (i in 0 until moviesArray.length()) {
-                    val item = moviesArray.getJSONObject(i)
-                    val title = item.getString("title_english")
-                    val imgUrl = item.getString("large_cover_image")
-                    val imdb_code = item.getString("imdb_code")
-                    val type = "movie"
-                    movies.add(MovieItem(title, imgUrl, imdb_code, type))
-                }
+
+
+
+
+
 
                 withContext(Dispatchers.Main) {
-                    val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-                    recyclerView.layoutManager = GridLayoutManager(this@MainActivity, 2)
-                    recyclerView.adapter = GridAdapter(movies)
-                    val spacing = (19 * resources.displayMetrics.density).toInt() // 16dp to px
-                    recyclerView.addItemDecoration(EqualSpaceItemDecoration(spacing))
+
+                            val  backdrop_Widget = findViewById<ImageView>(R.id.backdropImageView)
+                            val  poster_widget = findViewById<ImageView>(R.id.posterImageView)
+                            val  title_widget = findViewById<TextView>(R.id.title_widget)
+                            val  year_widget = findViewById<TextView>(R.id.year_widget)
+                            val  Rating_widget = findViewById<TextView>(R.id.Rating_widget)
+                            val  Overview_widget = findViewById<TextView>(R.id.overview_widget)
+
+
+
+                            title_widget.text = original_title
+                            year_widget.text = release_date
+                            Rating_widget.text  = "⭐ ${vote_average}/10"
+                            Overview_widget.text = overview
+
+
+                            Picasso.get()
+                                .load(backdrop_Url)
+                                .fit()
+                                .centerInside()
+                                .into(backdrop_Widget)
+
+                            Picasso.get()
+                                .load(poster_Url)
+                                .fit()
+                                .centerInside()
+                                .into(poster_widget)
                 }
 
-                 */
+
             } catch (e: Exception) {
                 Log.e("DEBUG_TAG", "Error fetching data", e)
             }
